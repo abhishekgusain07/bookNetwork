dfb908f4a6ac3ee0b0a2b80064d8334db4045e8d
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2025-01-07 13:41:16 +0530

    add: securityFilterChain (jwt authentication)

---
+++ book-network-api/src/main/java/com/gusain/book/security/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package com.gusain.book.security;
+
+import com.gusain.book.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("No user found with username: "+username));
+    }
+}
