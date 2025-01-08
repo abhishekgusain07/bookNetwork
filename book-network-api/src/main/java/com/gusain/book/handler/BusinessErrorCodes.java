ead4c09092a380a42b2345a4202a0259c07977da
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2025-01-08 09:59:33 +0530

    added: exception handling

---
+++ book-network-api/src/main/java/com/gusain/book/handler/BusinessErrorCodes.java
@@ -0,0 +1,29 @@
+package com.gusain.book.handler;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+public enum BusinessErrorCodes {
+
+    NO_CODE(0, HttpStatus.NOT_IMPLEMENTED, "No code"),
+    INCORRECT_CURRENT_PASSWORD(300, HttpStatus.BAD_REQUEST, "Incorrect current password"),
+    NEW_PASSWORD_DOES_NOT_MATCH(301, HttpStatus.BAD_REQUEST, "The New Password does not match"),
+    ACCOUNT_LOCKED(302, HttpStatus.FORBIDDEN, "User account locked"),
+    ACCOUNT_DISABLED(303, HttpStatus.FORBIDDEN, "User account is disabled"),
+    BAD_CREDENTIALS(304, HttpStatus.FORBIDDEN, "Login username or password is incorrect");
+
+    @Getter
+    private final int code;
+
+    @Getter
+    private final String description;
+
+    @Getter
+    private final HttpStatus httpStatus;
+
+    BusinessErrorCodes(int code, HttpStatus httpStatus, String description) {
+        this.code = code;
+        this.description = description;
+        this.httpStatus = httpStatus;
+    }
+}
---
+++ book-network-api/src/main/java/com/gusain/book/handler/GlobalExceptionHandler.java
@@ -0,0 +1,97 @@
+package com.gusain.book.handler;
+
+import jakarta.mail.MessagingException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = LockedException.class)
+    public ResponseEntity<ExceptionResponse> handleException(LockedException exception) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(
+                        ExceptionResponse.builder()
+                                .businessErrorCode(BusinessErrorCodes.ACCOUNT_LOCKED.getCode())
+                                .businessErrorDescription(BusinessErrorCodes.ACCOUNT_LOCKED.getDescription())
+                                .error(exception.getMessage())
+                                .build()
+                );
+    }
+
+    @ExceptionHandler(value= DisabledException.class)
+    public ResponseEntity<ExceptionResponse> handleException(DisabledException exception) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(
+                        ExceptionResponse.builder()
+                                .businessErrorCode(BusinessErrorCodes.ACCOUNT_DISABLED.getCode())
+                                .businessErrorDescription(BusinessErrorCodes.ACCOUNT_DISABLED.getDescription())
+                                .error(exception.getMessage())
+                                .build()
+                );
+    }
+
+    @ExceptionHandler(value= BadCredentialsException.class)
+    public ResponseEntity<ExceptionResponse> handleException(BadCredentialsException exception) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(
+                        ExceptionResponse.builder()
+                                .businessErrorCode(BusinessErrorCodes.BAD_CREDENTIALS.getCode())
+                                .businessErrorDescription(BusinessErrorCodes.BAD_CREDENTIALS.getDescription())
+                                .error(BusinessErrorCodes.BAD_CREDENTIALS.getDescription())
+                                .build()
+                );
+    }
+
+    @ExceptionHandler(value= MessagingException.class)
+    public ResponseEntity<ExceptionResponse> handleException(MessagingException exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(
+                        ExceptionResponse.builder()
+                                .error(exception.getMessage())
+                                .build()
+                );
+    }
+
+    @ExceptionHandler(value= MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> handleException(MethodArgumentNotValidException exception) {
+        Set<String> errors = new HashSet<>();
+        exception.getBindingResult().getAllErrors()
+                .forEach(error -> errors.add(error.getDefaultMessage()));
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(
+                        ExceptionResponse.builder()
+                                .validationErrors(errors)
+                                .build()
+                );
+    }
+
+    @ExceptionHandler(value= Exception.class)
+    public ResponseEntity<ExceptionResponse> handleException(Exception exception) {
+        //todo: logging mechanism
+        exception.printStackTrace();
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(
+                        ExceptionResponse.builder()
+                                .businessErrorDescription("Internal Error, contact the admin")
+                                .error(exception.getMessage() )
+                                .build()
+                );
+    }
+
+}
